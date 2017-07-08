c42b506667ac2b3bf62166c4ba55a3cd95bb16b5
Evan Chang <evanmchang@gmail.com>
2017-07-08 13:18:19 -0700

    Created Decks class

---
+++ Decks.py
@@ -0,0 +1,19 @@
+import Card
+
+class Decks:
+    
+    def __init__(self, numOfDecks: int):
+        
+        self._numOfDecks = numOfDecks
+        self._cardList = []
+        self._setUp()
+        
+    def _setUp(self):
+        for i in range(0, self._numOfDecks):
+            for j in ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']:
+                c = Card(j)
+                self._cardList.append(c)
+        self._
+                
+    def getCount(self):
+        
